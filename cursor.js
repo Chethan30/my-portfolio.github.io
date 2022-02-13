c616805a5af8b6befd3855659a29655a14d6ace4
Chethan30 <chetan611611611@gmail.com>
2022-02-13 16:51:16 +0530

    first commit

---
+++ cursor.js
@@ -0,0 +1,22 @@
+let cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+
+  cursor.style.left = `${x}px`;
+  cursor.style.top = `${y}px`;
+}
+
+let links = Array.from(document.querySelectorAll("a"));
+
+links.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    cursor.classList.add("grow");
+  });
+  link.addEventListener("mouseleave", () => {
+    cursor.classList.remove("grow");
+  });
+});
